81e9964ebe14d567245a25b33ae83d314f85173d
Patrick Dayton <pdayton@fastrobot.com>
2017-05-11 11:34:01 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# docker_book
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: docker_book
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
